a53ffba8f9f82808bac97ba89092578267edfb58
PatrickKubica <patrickkubica@gmx.de>
2017-12-21 11:26:19 +0100

    add bootstrap and basic navigation
    - add bootstrap include and build basic navigation
    - add layout file and extend it in the available views

--- routes/movies.js
+++ routes/movies.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/show', (req, res) => {
-  res.render('movies_show');
+router.get('/list', (req, res) => {
+  res.render('movies_list');
+});
+
+router.get('/add', (req, res) => {
+  res.render('movies_add');
 });
 
 module.exports = router;
